0ef0965db25e15e7edeec18c110e5511c4210bd1
Tor Petersen <tor.arnth.petersen@gmail.com>
2020-03-29 12:38:49 +0200

    Revert "changes"
    This reverts commit d4aea55ae9fe0ca1ccd660f1cedca23064735257.

--- CV/cv.py
+++ CV/cv.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-source_image = cv2.imread('greens.png')
+source_image = cv2.imread('CV/greens.png')
 
 img_HSV = cv2.cvtColor(source_image, cv2.COLOR_BGR2HSV)
 lower_threshold = np.array([35, 0, 0])
@@ -9,9 +9,7 @@ upper_threshold = np.array([85, 255, 255])
 
 img_thresholded = cv2.inRange(img_HSV, lower_threshold, upper_threshold)
 
-cv2.imwrite('alpha.png', img_thresholded)
-
-#cv2.imshow('res', img_thresholded)
+cv2.imwrite('CV/alpha.png', img_thresholded)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
