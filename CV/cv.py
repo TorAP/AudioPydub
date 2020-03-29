d5ecb7eea15e5d9ed3e27e7d20783bc8d7a4d70a
Tor Petersen <tor.arnth.petersen@gmail.com>
2020-03-29 12:41:10 +0200

    Revert "stuff"
    This reverts commit 4133cebc000522eed4133a8fa73ae2fa95b87df9.

--- CV/cv.py
+++ CV/cv.py
@@ -1,34 +1,17 @@
 import numpy as np
 import cv2
-from scipy import ndimage
 
-source_vid = cv2.VideoCapture('CV/input.mp4')
+source_image = cv2.imread('greens.png')
 
-while True:
-    _, frame = source_vid.read()
+img_HSV = cv2.cvtColor(source_image, cv2.COLOR_BGR2HSV)
+lower_threshold = np.array([35, 0, 0])
+upper_threshold = np.array([85, 255, 255])
 
-    img_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_threshold = np.array([35, 50, 50])
-    upper_threshold = np.array([85, 255, 255])
+img_thresholded = cv2.inRange(img_HSV, lower_threshold, upper_threshold)
 
-    img_thresholded = cv2.inRange(img_HSV, lower_threshold, upper_threshold)
+cv2.imwrite('alpha.png', img_thresholded)
 
-    img_eroded = cv2.erode(img_thresholded, np.ones((25, 25), np.uint8))
-    img_closed = cv2.dilate(img_eroded, np.ones((25, 25), np.uint8))
+#cv2.imshow('res', img_thresholded)
 
-    contours, _ = cv2.findContours(img_closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-    for c in contours:
-        approx = cv2.approxPolyDP(c, 4, True)
-        cv2.drawContours(frame, [approx], 0, (0, 200, 0), 2)
-        #center_of_mass = ndimage.measurements.center_of_mass(approx)
-        #cv2.circle(img=frame, center=center_of_mass, radius=20, color=(200, 0, 0))
-
-    cv2.imshow('res', frame)
-
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
-
-source_vid.release()
+cv2.waitKey(0)
 cv2.destroyAllWindows()
--- CV/target.py
+++
@@ -1,6 +0,0 @@
-from scipy import ndimage
-
-class Target():
-    def __init__(self, contours):
-        self.contours = contours
-        
