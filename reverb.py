ef028dcaccab4a524a52b5f91d2089f6609c008f
Tor arnth  Petersen <torarnthpetersen@MacBook-Pro.local>
2020-04-17 11:35:43 +0200

    pydub  changes

--- reverb.py
+++ reverb.py
@@ -1,23 +1,10 @@
-# %matplotlib inline
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.io.wavfile as wave
-import IPython.display as ipd
-import matplotlib.pyplot as plt
-from scipy import signal
+import urllib.request
+from pydub import AudioSegment
+from pydub.playback import play
+# Download an audio file
+urllib.request.urlretrieve("https://tinyurl.com/wx9amev", "metallic-drums.wav")
+# Load into PyDub
+loop = AudioSegment.from_wav("metallic-drums.wav")
 
-# load a trumpet signal
-samplingFreq,cleanTrumpetSignal = wave.read('flute-c5-sines.wav')
-
-# cleanTrumpetSignal = cleanTrumpetSignal/2**15 # normalise
-
-samplingFreqChurch,churchImpulse = wave.read('MEDIUM DAMPING ROOM E001 M2S.wav')
-
-# sampling rate 16 kHz
-cleanTrumpetSignal = np.reshape(cleanTrumpetSignal, cleanTrumpetSignal.size)
-# sampling rate 96 kHz
-churchImpulse =np.reshape(churchImpulse, churchImpulse.size)
-
-convoledSignal = np.convolve(churchImpulse,cleanTrumpetSignal,mode='valid')
-ipd.Audio(convoledSignal,rate =samplingFreq) 
+print(loop)
 
