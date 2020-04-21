57dfcd32c3ed708282e2e28e8e3838bd07efbb66
Tor arnth  Petersen <torarnthpetersen@MacBook-Pro.local>
2020-04-21 15:58:16 +0200

    Functions wokring - loading .wav and playing #

--- reverb.py
+++ reverb.py
@@ -1,15 +1,59 @@
 from pydub import AudioSegment
 from pydub.playback import play
-import numpy
+import numpy as np
+import scipy.signal as sps
+import scipy.io.wavfile as tryn
+from scipy.io.wavfile import write
+import simpleaudio.functionchecks as fc
+import simpleaudio as sa
+import wave
 
 
-raw_audio = AudioSegment.from_file("flute-c5-sines.wav", format="wav")
 
-raw_audio1 = AudioSegment.from_file("MEDIUM DAMPING ROOM E001 M2S.wav", format="wav")
+def convolution (waveFileOne,waveFileTwo):
+
+    samplingFreq1,cleanTrumpetSignal1 = tryn.read(waveFileOne)
+
+    samplingFreq2,cleanTrumpetSignal2 = tryn.read(waveFileTwo)
                                   
 
-raw_audio.set_sample_width(1)
+    ##Use both channels
+    leftSpeakerSignal=cleanTrumpetSignal2[:,0]
+    rightSpeakerSignal=cleanTrumpetSignal2[:,1]
+
+    scaled = np.convolve(cleanTrumpetSignal1,leftSpeakerSignal)
+
+    write('test.wav', 44100, scaled)
+    
+    #return write
 
 
-print(raw_audio1)
-play(raw_audio1)
+
+def convert(path):
+
+    #audio_data - object with audio data (must support the buffer interface)
+    #num_channels (int) – the number of audio channels
+    #bytes_per_sample (int) – the number of bytes per single-channel sample
+    #sample_rate (int) – the sample rate in Hz
+
+    wave_read = wave.open(path, 'rb')   
+    audio_data = wave_read.readframes(wave_read.getnframes())
+    num_channels = wave_read.getnchannels()
+    bytes_per_sample = wave_read.getsampwidth()
+    sample_rate = wave_read.getframerate()
+
+    wave_obj = sa.WaveObject(audio_data, num_channels, bytes_per_sample, sample_rate)
+
+    return wave_obj
+
+
+#convolution('flute-c5-sines.wav','MEDIUM DAMPING ROOM E001 M2S.wav')
+
+#lol = convert() 
+
+#lol.play()
+obj = convert('beat.wav')
+
+play_obj = obj.play()
+play_obj.wait_done()
+
