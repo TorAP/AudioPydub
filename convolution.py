458fc1f48be7da8eedc6ab0eb88fc6201ecc2f8a
Tor arnth  Petersen <torarnthpetersen@MacBook-Pro.local>
2020-04-23 09:30:31 +0200

    Cannot get an 1D array of signal for Convolve-func

--- convolution.py
+++ convolution.py
@@ -5,66 +5,84 @@ import scipy.signal as sps
 import scipy.io.wavfile as tryn
 from scipy.io.wavfile import write
 import simpleaudio.functionchecks as fc
-#AudioPlayer: https://simpleaudio.readthedocs.io/en/latest/simpleaudio.html
+# AudioPlayer: https://simpleaudio.readthedocs.io/en/latest/simpleaudio.html
 import simpleaudio as sa
 import wave
+from scipy import signal
 
 
+def convolution (waveFileOne,waveFileTwo):
 
-# #def convolution (waveFileOne,waveFileTwo):
+    samplingFreq1,cleanTrumpetSignal1 = tryn.read(waveFileOne)
 
-#     samplingFreq1,cleanTrumpetSignal1 = tryn.read(waveFileOne)
+    samplingFreq2,cleanTrumpetSignal2 = tryn.read(waveFileTwo)
 
-#     samplingFreq2,cleanTrumpetSignal2 = tryn.read(waveFileTwo)
-                                  
 
-#     ##Use both channels
-#     leftSpeakerSignal=cleanTrumpetSignal2[:,0]
-#     rightSpeakerSignal=cleanTrumpetSignal2[:,1]
+    ##Use both channels
+    # leftSpeakerSignal=cleanTrumpetSignal2[:,0]
+    # rightSpeakerSignal=cleanTrumpetSignal2[:,1]
 
-#     scaled = np.convolve(cleanTrumpetSignal1,leftSpeakerSignal)
+    scaled = np.convolve(cleanTrumpetSignal1,cleanTrumpetSignal2)
 
-#     write('test.wav', 44100, scaled)
-    
-#     #return write
+    print(cleanTrumpetSignal1,cleanTrumpetSignal2)
 
+    write('test2.wav', 44100, scaled)
+
+    return write
+
+convolution('IR.wav','pluck.wav')
 
 
 def convert(path):
 
+    # audio_data - object with audio data (must support the buffer interface)
+    # num_channels (int) – the number of audio channels
+    # bytes_per_sample (int) – the number of bytes per single-channel sample
+    # sample_rate (int) – the sample rate in Hz
 
-    #audio_data - object with audio data (must support the buffer interface)
-    #num_channels (int) – the number of audio channels
-    #bytes_per_sample (int) – the number of bytes per single-channel sample
-    #sample_rate (int) – the sample rate in Hz
-
-    wave_read = wave.open(path, 'rb')   
+    wave_read = wave.open(path, 'rb')
     audio_data = wave_read.readframes(wave_read.getnframes())
     num_channels = wave_read.getnchannels()
     bytes_per_sample = wave_read.getsampwidth()
     sample_rate = wave_read.getframerate()
 
-    wave_obj = sa.WaveObject(audio_data, num_channels, bytes_per_sample, sample_rate)
+    wave_obj = sa.WaveObject(audio_data, num_channels,
+                             bytes_per_sample, sample_rate)
 
     return wave_obj
 
-def convolution(length1:sa.WaveObject,length2:sa.WaveObject):
 
-    #audio_array = obj.astype(np.int16)
-    final = np.convolve(len(length1.audio_data),len(length2.audio_data))
-    print(final)
+def convolution(length1: sa.WaveObject, length2: sa.WaveObject):
 
-    num_channels = length1.num_channels
-    bytes_per_sample = length1.bytes_per_sample
-    sample_rate = length1.sample_rate
-    convoluted = sa.WaveObject(final, num_channels, bytes_per_sample, sample_rate)
+    # thefirst = len(length1.audio_data).astype(np.int16)
+    # thesecond = len(length2.audio_data).astype(np.int16)
+
+    # thesecond = np.int64(len(length2.audio_data))
+
+    # thesecond = (thesecond.astype(np.int32))
+
+    # thefirst = np.int64(len(length1.audio_data))
+
+    # thefirst = (thefirst.astype(np.int32))
+
+    audio_array = length1.audio_data.astype(np.int16)
+
+
+    print(length1.audio_data, length2.audio_data)
+
+    final = signal.oaconvolve(thefirst, thesecond)
+
+    num_channels = length2.num_channels
+    bytes_per_sample = length2.bytes_per_sample
+    sample_rate = length2.sample_rate
+    convoluted = sa.WaveObject(
+        final, num_channels, bytes_per_sample, sample_rate)
 
     return convoluted
 
 
 obj = convert('IR.wav')
-obj1 = convert('pluck.wav')
-
+obj1 = convert('flute-c5-sines.wav')
 
 
 # play_obj = obj.play()
@@ -72,17 +90,14 @@ obj1 = convert('pluck.wav')
 
 # final = np.convolve(len(obj.audio_data),len(obj1.audio_data))
 
-obj2 = convolution(obj1,obj)
+obj2 = convolution(obj, obj1)
 
 play_obj2 = obj2.play()
 play_obj2.wait_done()
 
 
-
-
-
 #audio_array = obj.astype(np.int16)
-#fc.LeftRightCheck.run()
+# fc.LeftRightCheck.run()
 
 # final = np.convolve(len(obj.audio_data),len(obj1.audio_data))
 
@@ -92,4 +107,4 @@ play_obj2.wait_done()
 
 # wave_obj1.play()
 
-#obj.__new__()
+# obj.__new__()
